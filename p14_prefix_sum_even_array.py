52111f8acfa230acf6691e0c05eafcda00844394
Neeraj Yadav <neerajasl05@gmail.com>
2024-11-07 05:25:15 +0530

    prefix even sum array

---
+++ p14_prefix_sum_even_array.py
@@ -0,0 +1,16 @@
+def create_even_prefix_sum(given_array):
+    prefix_sum = [None]*len(given_array)
+    for i in range(0,len(given_array)):
+        if i == 0:
+            prefix_sum[i] = given_array[i]
+        elif i%2 == 0:
+            prefix_sum[i] = prefix_sum[i - 1] + given_array[i]
+        else:
+            prefix_sum[i] = prefix_sum[i-1]
+    return prefix_sum
+
+if __name__ == "__main__":
+    l = list(map(int, input("Enter list of numbers:").split()))
+    sum = create_even_prefix_sum(l)
+    print(sum)
+        
