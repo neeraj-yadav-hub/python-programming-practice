4166f86f99cf0c3af6fdc9ea252e9939a82f685e
Neeraj Yadav <neerajasl05@gmail.com>
2024-11-07 05:43:07 +0530

    prefix odd sum array

--- p15_prefix_sum_odd_array.py
+++ p15_prefix_sum_odd_array.py
@@ -1,14 +1,9 @@
-"""
-Given N array elements. Create a prefix even odd array. A prefix even odd array is an array of
-sum of even indexes of an array.
-"""
-
-def create_even_prefix_sum(given_array):
+def create_odd_prefix_sum(given_array):
     prefix_sum = [None]*len(given_array)
     for i in range(0,len(given_array)):
-        if i == 0:
+        if i in [0,1]:
             prefix_sum[i] = given_array[i]
-        elif i%2 == 0:
+        elif i%2 != 0:
             prefix_sum[i] = prefix_sum[i - 1] + given_array[i]
         else:
             prefix_sum[i] = prefix_sum[i-1]
@@ -16,6 +11,5 @@ def create_even_prefix_sum(given_array):
 
 if __name__ == "__main__":
     l = list(map(int, input("Enter list of numbers:").split()))
-    sum = create_even_prefix_sum(l)
+    sum = create_odd_prefix_sum(l)
     print(sum)
-        
